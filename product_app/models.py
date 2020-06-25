fef35bae0a3e57c16fc1c742b04b3f199f198dcc
반현랑 <hyunlang@banhyeonlang-ui-MacBookAir.local>
2020-06-25 16:21:36 +0900

    [FIX] 스타일 가이드에 맞춰 모델 수정 - 필드명 복수에서 단수로 수정 - on_delete 속성을 CASCADE를 SET_NULL로 수정 - Product 테이블의 price를 Integer에서 Decimal로 변경 - 전체적인 Align 수정

--- product_app/models.py
+++ product_app/models.py
@@ -10,42 +10,49 @@ class Menu(models.Model):
         db_table = 'menus'
     
 class MenuCategory(models.Model):
-    menus      = models.ForeignKey(Menu, on_delete = models.CASCADE)
-    categories = models.ForeignKey('Category', on_delete = models.CASCADE)
+    menu     = models.ForeignKey('Menu', on_delete = models.SET_NULL, null = True)
+    category = models.ForeignKey('Category', on_delete = models.SET_NULL, null = True)
+    
+    class Meta:
+        db_table = 'menus_categories'
 
 class Category(models.Model):
-    name  = models.CharField(max_length = 100)
-    menus = models.ManyToManyField(Menu, through = 'MenuCategory')
+    name = models.CharField(max_length = 100)
+    menu = models.ManyToManyField('Menu', through = 'MenuCategory')
     
     def __str__(self):
-        return f'name: {self.name}, menus: {menus}'
+        return f'name: {self.name}, menus: {menu}'
+    
     class Meta:
         db_table = 'categories'
 
 class CategorySubcategory(models.Model):
-    categories     = models.ForeignKey(Category, on_delete = models.CASCADE)
-    sub_categoreis = models.ForeignKey('SubCategory', on_delete = models.CASCADE)
+    category     = models.ForeignKey('Category', on_delete = models.SET_NULL, null = True)
+    sub_category = models.ForeignKey('SubCategory', on_delete = models.SET_NULL, null = True)
+
+    class Meta:
+        db_table = 'cateogries_subcategories'
 
 class SubCategory(models.Model):
-    name         = models.CharField(max_length = 100)
-    descriptions = models.CharField(max_length = 1000)
-    categories   = models.ManyToManyField(Category, through = 'CategorySubcategory')
+    name        = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 1000)
+    category    = models.ManyToManyField('Category', through = 'CategorySubcategory')
     
     def __str__(self):
-        return f'name: {self.name}, categories: {self.categories}'
+        return f'name: {self.name}, categories: {self.category}'
+
     class Meta:
         db_table = 'sub_categories'
 
-
 class Size(models.Model):
-    name = models.CharField(max_length=20, default = 0, unique = True)
-    
+    name            = models.CharField(max_length=20, default = 0, unique = True)
+    product_color   = models.ManyToManyField('ProductColor', through = 'ProductColorSize')
+
     def __str__(self):
         return self.name
 
     class Meta:
         db_table = 'sizes'
-
 
 class Material(models.Model):
     name = models.CharField(max_length = 100, unique = True)
@@ -56,20 +63,18 @@ class Material(models.Model):
     class Meta:
         db_table = 'materials'
 
-
 class Country(models.Model):
     name = models.CharField(max_length = 100, unique = True)
 
     class Meta:
         db_table = 'countries'
 
-
 class Product(models.Model):
     name           = models.CharField(max_length = 200, unique = True)
-    price          = models.IntegerField()
-    materials      = models.ForeignKey(Material, on_delete = models.CASCADE)
-    countries      = models.ForeignKey(Country, on_delete = models.CASCADE)
-    sub_categories = models.ForeignKey(SubCategory, on_delete = models.CASCADE)
+    price          = models.DecimalField(max_digits = 12, decimal_places = 0)
+    material       = models.ForeignKey('Material', on_delete = models.SET_NULL, null = True)
+    country        = models.ForeignKey('Country', on_delete = models.SET_NULL, null = True)
+    sub_category   = models.ForeignKey('SubCategory', on_delete = models.SET_NULL, null = True)
     
     def __str__(self):
         return f'name: {self.name}, price: {self.price}'
@@ -77,10 +82,9 @@ class Product(models.Model):
     class Meta:
         db_table = 'products'
 
-
 class Color(models.Model):
     name     = models.CharField(max_length = 100, unique = True)
-    products = models.ManyToManyField(Product, through = 'ProductColor')
+    product  = models.ManyToManyField('Product', through = 'ProductColor')
 
     class Meta:
         db_table = 'colors'
@@ -88,36 +92,26 @@ class Color(models.Model):
     def __str__(self):
         return self.name
 
-
 class ProductColor(models.Model):
-    colors         = models.ForeignKey(Color, on_delete = models.CASCADE)
-    products       = models.ForeignKey(Product, on_delete = models.CASCADE)
+    color          = models.ForeignKey('Color', on_delete = models.SET_NULL, null = True)
+    product        = models.ForeignKey('Product', on_delete = models.SET_NULL, null = True)
     product_number = models.IntegerField(unique = True)
-    # user           = models.ManyToManyField(User, through='Review')
+    # user           = models.ManyToManyField('User', through='Review')
 
     class Meta:
         db_table = 'products_colors'
 
-
-class Reveiw(models.Model):
-    # user            = models.ForeignKey(User, on_delete = models.CASCADE)
-    products_colosr = models.ForeignKey(ProductColor, on_delete = models.CASCADE)
-    title           = models.CharField(max_length=100)
-    contents        = models.CharField(max_length=1000)
-    image_url       = models.CharField(max_length=1000)
-
-
-class ProductSize(models.Model):
-    products_colors   = models.ManyToManyField(ProductColor)
-    sizes             = models.ForeignKey(Size, on_delete = models.CASCADE)
+class ProductColorSize(models.Model):
+    product_color    = models.ForeignKey('ProductColor', on_delete = models.SET_NULL, null = True)
+    size             = models.ForeignKey(Size, on_delete = models.SET_NULL, null = True)
+    soldout          = models.BooleanField(default = False)
 
     class Meta:
         db_table = 'product_sizes'
 
-
 class Image(models.Model):
-    image_url           = models.CharField(max_length = 1000)
-    products_colors     = models.ForeignKey(ProductColor, on_delete = models.CASCADE)
+    image_url       = models.CharField(max_length = 1000)
+    product_color   = models.ForeignKey('ProductColor', on_delete = models.SET_NULL, null = True)
     
     def __self__(self):
         return self.image_url
@@ -125,24 +119,32 @@ class Image(models.Model):
     class Meta:
         db_table = 'images'
 
-
 class ListThumnail(models.Model):
-    image_url       = models.CharField(max_length=1000)
-    products_colors = models.ForeignKey(ProductColor, on_delete = models.CASCADE)
-
+    image_url     = models.CharField(max_length = 1000)
+    product_color = models.ForeignKey('ProductColor', on_delete = models.SET_NULL, null = True)
+    
+    class Meta:
+        db_table = 'list_thumnails'
 
 class ProductcolorProductthumnail(models.Model):
-    products_colors   = models.ForeignKey(ProductColor, on_delete = models.CASCADE)
-    product_thumnails = models.ForeignKey('ProductThumnail', on_delete = models.CASCADE)
+    product_color    = models.ForeignKey('ProductColor', on_delete = models.SET_NULL, null = True)
+    product_thumnail = models.ForeignKey('ProductThumnail', on_delete = models.SET_NULL, null = True)
 
-
+    class Meta:
+        db_table = 'productcolor_productthumnail'
+        
 class ProductThumnail(models.Model):
     image_url = models.CharField(max_length=1000)
 
+    class Meta:
+        db_table = 'product_thumnails'
 
-class Review(models.Model):
-    # user = models.ForeignKey(User, on_delete = models.CASCADE)
-    products_colors = models.ForeignKey(ProductColor, on_delete = models.CASCADE)
-    title = models.CharField(max_length=100)
-    contents = models.CharField(max_length=1000)
-    image_url = models.CharField(max_length=1000, null = True)
+class Reveiw(models.Model):
+    # user            = models.ForeignKey(User, on_delete = models.CASCADE)
+    product_color = models.ForeignKey('ProductColor', on_delete = models.SET_NULL, null = True)
+    title         = models.CharField(max_length=100)
+    content       = models.CharField(max_length=1000)
+    image_url     = models.CharField(max_length=1000)
+    
+    class Meta:
+        db_table = 'reviews'
