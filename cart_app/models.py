7d7a30c360d7acd82b252a91876a6d200a4c01e6
nanaon <naon.jeong@gmail.com>
2020-06-26 20:29:44 +0900

    1 [Fix] models.py 일부 필드에 걸려있던 주석 제거 - 다른 앱과 연결된 필드에 걸어놨던  주석 제거

--- cart_app/models.py
+++ cart_app/models.py
@@ -1,11 +1,11 @@
 from django.db  import models
  
 class Order(models.Model):
-    #user           = models.ForeignKey('users.User', on_delete = models.CASCADE)
+    user           = models.ForeignKey('users.User', on_delete = models.CASCADE)
     total_price    = models.DecimalField(max_digits = 12, decimal_places = 2)
     discount_price = models.DecimalField(max_digits = 12, decimal_places = 2, null = True, blank = True)
     final_price    = models.DecimalField(max_digits = 12, decimal_places = 2)
-    #product_color  = models.ManyToManyField('ProductColor', through = 'Cart')
+    product_color  = models.ManyToManyField('ProductColor', through = 'Cart')
     
     def __str__(self):
         return f'total: {self.total_price}, discount: {self.discount_price}, final: {self.final_price}'
@@ -14,7 +14,7 @@ class Order(models.Model):
         db_table = 'orders'
 
 class Cart(models.Model):
-    #product_color = models.ForeignKey('ProductColor', on_delete = models.CASCADE)
+    product_color = models.ForeignKey('ProductColor', on_delete = models.CASCADE)
     order         = models.ForeignKey('Order', on_delete = models.SET_NULL, null = True)
     quantity      = models.IntegerField()
 
