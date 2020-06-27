bb0903cd59d7d93c782b75e700a0e84ba6c5bc89
김동건 <dgk2481@gmail.com>
2020-06-27 19:44:36 +0900

    [Fix] 줄 바꿈 정렬 - 붙어있던 줄을 한 줄씩 띄움

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ import json
 import jwt
 import re
 import bcrypt
+
 from django.shortcuts import render
 from django.views import View
 from django.core.validators import validate_email
@@ -10,11 +11,13 @@ from django.http import (
     JsonResponse,
     HttpResponse
 )
+
 from dr_martens.settings import (
     SECRET_KEY,
     ALGORITHM
 )
 from users.models import User
+
 class SignUpView(View):
     def post(self, request):
         data = json.loads(request.body)
@@ -22,10 +25,13 @@ class SignUpView(View):
             validate_email(data['email'])
             if User.objects.filter(nickname = data['nickname']).exists():
                 return HttpResponse(status = 401)
+
             if re.match("^[0-9A-Za-z]{6,20}$", data['nickname']) == None:
                 return HttpResponse(status = 401)
+
             if re.match("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d$@$!%*#?&]{6,20}$", data['password']) == None:
                 return HttpResponse(status = 401)
+
             hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
             User(
                 name         = data['name'],
@@ -36,10 +42,13 @@ class SignUpView(View):
                 phone_number = data['phone_number'],
             ).save()
             return HttpResponse(status = 200)
+
         except ValidationError:
             return JsonResponse({'message' : 'INVALID_EMAIL'}, status = 400)
+
         except KeyError as e:
             return JsonResponse({'message' : str(e) + ' is right key name.'}, status = 400)
+
 class SignInView(View):
     def post(self, request):
         data = json.loads(request.body)
@@ -49,6 +58,8 @@ class SignInView(View):
                 if bcrypt.checkpw(data['password'].encode('utf-8'), users.password.encode('utf-8')):
                     token = jwt.encode({'users_id' : users.id}, SECRET_KEY, algorithm = ALGORITHM)
                     return JsonResponse({'access_token':token.decode('utf-8')}, status = 200)
+
                 return HttpResponse(status = 401)
+                
         except KeyError as e:
             return JsonResponse({'message' : str(e) + ' is right key name.'}, status = 400)
