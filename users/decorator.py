dd7045b505c6d4065f97504c8713b2220a9dc0a2
김동건 <dgk2481@gmail.com>
2020-06-29 14:51:02 +0900

    [Fix] decorator.py 변수명 수정, 사용하지 않는 코드 삭제 - users -> user로 users_id -> user_id로 수정 - from django.http import HttpResponse 삭제 - except jwt.ExpiredSignatureError : return JsonResponse({'message' : 'INVALID_USER'}, status = 400) 삭제 - DecodeError 에러메세지 INVALID_USER에서 INVALID_TOKEN로 수정

--- users/decorator.py
+++ users/decorator.py
@@ -1,10 +1,8 @@
 import jwt
 import json
 
-from django.http import (
-    JsonResponse,
-    HttpResponse
-)
+from django.http import JsonResponse
+
 
 from users.models import User
 from dr_martens.settings import (
@@ -17,17 +15,14 @@ def login_check(func):
         access_token = request.headers.get("Authorization", None)
         if access_token is not None:
             try:
-                decode_token  = jwt.decode(access_token, SECRET_KEY, algorithms = ALGORITHM)
-                users_id      = decode_token['users_id']
-                users         = User.objects.get(id = users_id)
-                request.users = users
+                decode_token = jwt.decode(access_token, SECRET_KEY, algorithms = ALGORITHM)
+                user_id      = decode_token['user_id']
+                user         = User.objects.get(id = user_id)
+                request.user = user
                 return func(self, request, *args, **kwargs)
 
             except jwt.DecodeError:
-                return JsonResponse({'message' : 'INVALID_USER'}, status = 400)
-
-            except jwt.ExpiredSignatureError:
-                return JsonResponse({'message' : 'INVALID_USER'}, status = 400)
+                return JsonResponse({'message' : 'INVALID_TOKEN'}, status = 400)
 
             except User.DoesNotExist:
                 return JsonResponse({'message' : 'INVALID_USER'}, status = 400)
