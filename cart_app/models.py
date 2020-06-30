1b2d19d4da0093e0f2e1e8576caf536b422e48b4
nanaon <naon.jeong@gmail.com>
2020-06-30 19:37:53 +0900

    [Add] cart 앱 models.py 에 새 테이블 추가 - 주문 중인 상품만 카트에서 보여주기 위해 order_status 테이블 추가 - orders 테이블이 order_status 테이블을 참조하도록 연결

--- cart_app/models.py
+++ cart_app/models.py
@@ -7,10 +7,16 @@ class Order(models.Model):
     size           = models.CharField(max_length = 30)
     product_image  = models.URLField(max_length = 1000)
     quantity       = models.IntegerField(default = 1)
+    order_status   = models.ForeignKey('OrderStatus', on_delete = models.SET_NULL, null = True)
     product_color  = models.ManyToManyField('product_app.ProductColor', through = 'Cart')
 
     class Meta:
         db_table = 'orders'
+
+class OrderStatus(models.Model):
+    name = models.CharField(max_length = 30)
+    class Meta:
+        db_table = 'order_status'
 
 class Cart(models.Model):
     product_color = models.ForeignKey('product_app.ProductColor', on_delete = models.CASCADE)
