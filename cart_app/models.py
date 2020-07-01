8f2cf8e4fe54fedf9d3058b633380c07f9796339
nanaon <naon.jeong@gmail.com>
2020-07-01 14:43:08 +0900

    [Fix] cart 앱 models.py 테이블 수정 - orders 테이블과 carts 테이블에 섞여있던 주문 상품 정보(product_color, size, quantity)를 carts 테이블로 옮김

--- cart_app/models.py
+++ cart_app/models.py
@@ -4,9 +4,6 @@ class Order(models.Model):
     user           = models.ForeignKey('users.User', on_delete = models.CASCADE)
     total_price    = models.DecimalField(max_digits = 12, decimal_places = 0)
     final_price    = models.DecimalField(max_digits = 12, decimal_places = 0)
-    size           = models.CharField(max_length = 30)
-    product_image  = models.URLField(max_length = 1000)
-    quantity       = models.IntegerField(default = 1)
     order_status   = models.ForeignKey('OrderStatus', on_delete = models.SET_NULL, null = True)
     product_color  = models.ManyToManyField('product_app.ProductColor', through = 'Cart')
 
@@ -22,6 +19,8 @@ class OrderStatus(models.Model):
 class Cart(models.Model):
     product_color = models.ForeignKey('product_app.ProductColor', on_delete = models.CASCADE)
     order         = models.ForeignKey('Order', on_delete = models.SET_NULL, null = True)
+    size          = models.CharField(max_length = 30)
+    quantity      = models.IntegerField(default = 1)
 
     class Meta:
         db_table = 'carts'
