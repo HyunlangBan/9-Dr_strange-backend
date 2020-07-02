eeffd06838096217a414e4ecc8486c81bd6388c4
nanaon <naon.jeong@gmail.com>
2020-07-02 15:22:25 +0900

    임시저장

--- cart_app/views.py
+++ cart_app/views.py
@@ -31,13 +31,23 @@ from cart_app.models        import (
 )
 
 class OrderView(View):
+    PENDING = 1
+    DEFAULT_LIKES = 600
+
     @login_check
     def post(self, request):
         try:
-            user    = request.user
-            user_id = user.id
-            data    = json.loads(request.body)
-            product_name           = data['productName']
+            user_id                = request.user.id
+            data                   = json.loads(request.body)
+            product                = ProductColor.objects.prefetch_related(
+                                    'product__color_set'
+                                    ).prefetch_related(
+                                    'detailimage_set'
+                                    ).get(product_number = product_number)
+            product_color_id       = product.id
+            product_id             = product.product.id
+            color_name             = product.color.name
+            product_image          = product.detailimage_set.first().image_url
             product_number         = data['productNum']
             total_price            = data['currentOrigin']
             total_sale_price       = data['currentSale']
@@ -45,40 +55,28 @@ class OrderView(View):
             size                   = data['currentSize']
             total_discounted_price = total_price - total_sale_price
             final_price            = total_price - total_discounted_price
-            product_sets           = ProductColor.objects.prefetch_related('product__color_set').prefetch_related('detailimage_set')
-            product_get            = product_sets.get(product_number = product_number)
-            product_color_id       = product_get.id
-            product_id             = product_get.product.id
-            color_name             = product_get.color.name
-            product_image          = product_get.detailimage_set.first().image_url   
-            
-            if not Order.objects.filter(user_id = user_id, order_status_id = 1).exists(): 
+          
+            if not Order.objects.filter(user_id = user_id, order_status_id = PENDING).exists(): 
                 Order(
                     user_id         = user_id,
                     total_price     = total_price,
                     final_price     = final_price,
-                    order_status_id = 1
+                    order_status_id = PENDING
                 ).save()
-                order_id = Order.objects.get(user_id = user_id, order_status_id = 1).id
-                Cart(
-                    product_color_id = product_color_id,
-                    order_id         = order_id,
-                    size             = size,
-                    quantity         = quantity
-                ).save()
+                order_id = Order.objects.get(user_id = user_id, order_status_id = PENDING).id
             else:
-                order              = Order.objects.get(user_id = user_id, order_status_id = 1)
+                order              = Order.objects.get(user_id = user_id, order_status_id = PENDING)
                 order.total_price  += total_price
                 order.final_price  += final_price
                 order.save()
                 order_id           = order.id
                 cart_items         = Cart.objects.filter(order_id = order_id)
-                Cart(
-                    product_color_id = product_color_id,
-                    order_id         = order_id,
-                    size             = size,
-                    quantity         = quantity
-                ).save()
+            Cart(
+                product_color_id = product_color_id,
+                order_id         = order_id,
+                size             = size,
+                quantity         = quantity
+            ).save()
             return HttpResponse(status = 200)
         
         except ObjectDoesNotExist:
@@ -90,12 +88,14 @@ class OrderView(View):
     
     @login_check
     def get(self, request):
-        user    = request.user
-        user_id = user.id
+        user_id = request.user.id
         try:
-            order_id     = Order.objects.get(user_id = user_id, order_status_id = 1)
-            cart_sets    = Cart.objects.prefetch_related('order__product_color__product__color_set').prefetch_related('order__user__userproductcolor_set')
-            cart_items   = cart_sets.filter(order_id = order_id) 
+            order_id     = Order.objects.get(user_id = user_id, order_status_id = PENDING)
+            cart_items   = Cart.objects.prefetch_related(
+                          'order__product_color__product__color_set'
+                          ).prefetch_related(
+                          'order__user__userproductcolor_set'
+                          ).filter(order_id = order_id) 
             product_list = [
                     {
                         "productName"       : item.product_color.product.name,
@@ -105,7 +105,7 @@ class OrderView(View):
                         "singleOriginPrice" : item.product_color.product.price * item.quantity,
                         "singleSalePrice"   : item.product_color.discount_price * item.quantity,
                         "quantity"          : item.quantity,
-                        "like"              : 600 + item.order.user.userproductcolor_set.count(),
+                        "like"              : DEFAULT_LIKES + item.order.user.userproductcolor_set.count()
                     }
             for item in cart_items]
             
