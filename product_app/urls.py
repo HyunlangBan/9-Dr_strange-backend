8e4eeb842ac39c87191f94abd1c15ff2d2b455a9
김동건 <dgk2481@gmail.com>
2020-07-02 21:14:04 +0900

    [Fix] ProductListView 수정(list comprehension, RESTful 등등) - from product_app.models import * 에서 from product_app.models import ProductColor로 수정 - RESTful 규칙적용해서 Querystring 사용 - for i in 에서 for single_product로 수정 - list comprehension으로 코드 수정 - prefetch_related사용하는 코드로 수정

--- product_app/urls.py
+++ product_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from product_app.views import ProductListView
 
 urlpatterns = [
-    path('/list/<str:menu_name>', ProductListView.as_view())
+    path('/list', ProductListView.as_view())
 ]
--- product_app/views.py
+++ product_app/views.py
@@ -3,25 +3,23 @@ import json
 from django.views import View
 from django.http  import JsonResponse
 
-from product_app.models import *
+from product_app.models import ProductColor
 
 class ProductListView(View):
-    def post(self, request, menu_name):
-        product_list = ProductColor.objects.filter(product__sub_category__category__menu__name=menu_name).distinct()
-        products = []
-        for i in product_list:
-            product = {}
-            product['subCategoryId'] = i.product.sub_category.distinct()[0].id
-            product['productNum'] = i.product_number
-            product['productName'] = i.product.name
-            product['like'] = 0
-            product['color'] = i.color.name
-            product['originPrice'] = i.product.price
-            product['salePrice'] = i.discount_price
-            images = i.productimage_set.all()
-            image = []
-            for s in images:
-                image.append(s.image_url)
-            product['productImg'] = image
-            products.append(product)
+    def get(self, request):
+        menu_name = request.GET.get('menu_name', None)
+        product_list = ProductColor.objects.prefetch_related('product__menu_category_sub_category__menu_category__menu')
+        gender_product = product_list.filter(product__menu_category_sub_category__menu_category__menu__name = menu_name).distinct()
+        DEFAULT_LIKES = 600
+        products = [
+            {
+                'subCategoryId' : single_product.product.menu_category_sub_category.first().sub_category.id,
+                'productNum'    : single_product.product_number,
+                'productName'   : single_product.product.name,
+                'like'          : DEFAULT_LIKES+single_product.userproductcolor_set.count(),
+                'color'         : single_product.color.name,
+                'originPrice'   : single_product.product.price,
+                'salePrice'     : single_product.discount_price,
+                'productImg'    : [img.image_url for img in single_product.productimage_set.all()]
+            } for single_product in gender_product ]
         return JsonResponse({'products' : products})
