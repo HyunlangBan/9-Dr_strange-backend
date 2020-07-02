2fa994f0faff88590713f68e8a6149577e1bd0f1
nanaon <naon.jeong@gmail.com>
2020-07-02 18:33:34 +0900

    [Fix] product 앱 테이블 내 새 필드 생성 - reviews 테이블에 order foreign key 필드 추가 - 배송 완료(order_status_id=3)인 order만 리뷰를 쓸 수 있게 하기 위함

--- product_app/models.py
+++ product_app/models.py
@@ -118,6 +118,7 @@ class Review(models.Model):
     content       = models.CharField(max_length=1000)
     image_url     = models.URLField(max_length=1000)
     stars         = models.IntegerField()
-    
+    order         = models.ForeignKey('cart_app.Order', on_delete = models.SET_NULL, null = True)
+
     class Meta:
         db_table = 'reviews'
